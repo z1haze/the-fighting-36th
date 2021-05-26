76137994d1cbbcd34616c031d2f5286d3c159e95
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-05-25 21:28:23 -0700

    working on hero

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,6 +1,18 @@
 const bs = require('bootstrap');
 
+const {WOW} = require('wowjs');
+
 [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     .map((el) => new bs.Popover(el));
+
+const wow = new WOW({
+    boxClass    : 'wow',
+    animateClass: 'animated',
+    offset      : 0,
+    mobile      : false,
+    live        : true
+});
+
+wow.init();
 
 console.log('Loaded! 😀'); // eslint-disable-line no-console
